0e9673af77b550ca189d6398e7b083433a26afc8
Piyush Saini <piyushs2k4@gmail.com>
2024-09-03 18:55:04 +0000

    Sync LeetCode submission Runtime - 6 ms (0.21%), Memory - 20.4 MB (100.00%)

---
+++ my-submissions/2614-maximum-count-of-positive-integer-and-negative-integer/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int ncount=0;
+        int pcount=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                pcount++;
+            }
+            else if(nums[i]<0){
+                ncount++;
+            }
+        }
+        return max(pcount,ncount);
+    }
+};
