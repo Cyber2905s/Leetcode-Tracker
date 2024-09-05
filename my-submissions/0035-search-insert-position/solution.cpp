89b092d6af9fc7eba4a8c408fd552f22e6aec4f7
Piyush Saini <piyushs2k4@gmail.com>
2024-09-05 21:28:10 +0000

    Sync LeetCode submission Runtime - 4 ms (0.19%), Memory - 12.3 MB (100.00%)

---
+++ my-submissions/0035-search-insert-position/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                return i;
+            }
+            else{
+                if(target>nums[n-1]){
+                    return n;
+                }
+                else if(target<nums[0]){
+                    return 0;
+                }
+                else if(nums[i]<target && target<nums[i+1]){
+                    return i+1;
+                }
+            }
+        }
+        return 0;
+        // vector<int> ans;
+        // ans=nums;
+        // ans.push_back(target);
+        // sort()
+        
+    }
+};
