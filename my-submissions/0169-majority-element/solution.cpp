05416a07a00f77eede38e08df18ab5ba7489e382
Piyush Saini <piyushs2k4@gmail.com>
2024-09-05 19:38:38 +0000

    Sync LeetCode submission Runtime - 1613 ms (6.17%), Memory - 26.9 MB (99.98%)

---
+++ my-submissions/0169-majority-element/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count>(n/2)){
+                return nums[i];
+            }
+            else{
+                count=0;
+                continue;
+            }
+        }
+        return 0;
+    }
+};
