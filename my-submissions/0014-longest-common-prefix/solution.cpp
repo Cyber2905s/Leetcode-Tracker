d86b762c832b8b13f28f5c401bf89181ba5514c5
Piyush Saini <piyushs2k4@gmail.com>
2024-09-12 17:44:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.4 MB (99.99%)

---
+++ my-submissions/0014-longest-common-prefix/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string ans = strs[0];
+        for (int i = 1; i < strs.size(); ++i) {
+            int k= min(ans.length(),strs[i].length());
+            int j=0;
+            while (j<k) {
+                if(ans[j] == strs[i][j]){
+                    j++;
+                }
+                else{
+                    break;
+                }
+            }
+            ans = ans.substr(0, j);
+            if (ans.empty()) return "";
+        }
+        return ans;
+    }
+};
