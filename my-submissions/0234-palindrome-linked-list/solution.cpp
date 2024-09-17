651df2e472e93dd1e7f8d2fc4fab3fc9f36b1a5b
Piyush Saini <piyushs2k4@gmail.com>
2024-09-17 16:44:40 +0000

    Sync LeetCode submission Runtime - 177 ms (5.12%), Memory - 120.8 MB (46.55%)

---
+++ my-submissions/0234-palindrome-linked-list/solution.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ ListNode* getmid(ListNode* head){
+    ListNode* slow=head;
+    ListNode* fast=head->next;
+    while(fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    return slow;
+}
+ListNode* Reverse(ListNode* head){
+    ListNode* curr=head;
+    ListNode* prev=NULL;
+    ListNode* next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head->next==NULL){
+            return true;
+        }
+        ListNode* mid=getmid(head);
+        ListNode* temp=mid->next;
+        mid->next=Reverse(temp);
+        ListNode* head1=head;
+        ListNode* head2=mid->next;
+        while(head2!=NULL){
+            if(head1->val!=head2->val){
+                return false;
+            }
+            else{
+                head1=head1->next;
+                head2=head2->next;
+            }
+        }
+        temp=mid->next;
+        mid->next=Reverse(temp);
+        return true;
+    }
+};
