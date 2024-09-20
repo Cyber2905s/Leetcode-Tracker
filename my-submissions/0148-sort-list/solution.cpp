19616b22bdd46bfa96a16afc768a7c4b765282d3
Piyush Saini <piyushs2k4@gmail.com>
2024-09-20 21:17:42 +0000

    Sync LeetCode submission Runtime - 138 ms (5.03%), Memory - 75 MB (42.76%)

---
+++ my-submissions/0148-sort-list/solution.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ ListNode* findmid(ListNode* head){
+    if(head==NULL||head->next==NULL){
+        return head;
+    }
+    ListNode* slow=head;
+    ListNode* fast=head->next;
+    while(fast!=NULL&&fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+ListNode* mergeLists(ListNode* list1,ListNode* list2){
+    ListNode* dummy=new ListNode(-1);
+    ListNode* temp=dummy;
+    while(list1!=NULL&&list2!=NULL){
+        if (list1->val <= list2->val) {
+            temp->next = list1;
+            list1 = list1->next;
+        } 
+        else {
+            temp->next = list2;
+            list2 = list2->next;
+        }
+        temp=temp->next;
+    }
+    if(list1!=NULL){
+        temp->next=list1;
+    }
+    else{
+        temp->next=list2;
+    }
+    return dummy->next;
+}
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL||head->next==NULL){
+            return head;
+        }
+        ListNode* middle=findmid(head);
+        ListNode* right=middle->next;
+        middle->next=NULL;
+        ListNode* left=head;
+        left=sortList(left);
+        right=sortList(right);
+        return mergeLists(left,right);
+    }
+};
