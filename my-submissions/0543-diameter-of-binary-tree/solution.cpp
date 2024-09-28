d62864a864d88ce716d6821854690a0a5afaadcc
Piyush Saini <piyushs2k4@gmail.com>
2024-09-28 16:38:25 +0000

    Sync LeetCode submission Runtime - 9 ms (17.67%), Memory - 22.3 MB (99.96%)

---
+++ my-submissions/0543-diameter-of-binary-tree/solution.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ /*int height(TreeNode* root){
+    if(root==NULL){
+        return 0;
+    }
+    int left=height(root->left);
+    int right=height(root->right);
+    return (max(left,right)+1);
+ }*/
+ pair<int,int> dfast(TreeNode* root){
+    if(root==NULL){
+        return make_pair(0,0);
+    }
+    pair<int,int> left=dfast(root->left);
+    pair<int,int> right=dfast(root->right);
+    int left1=left.first;
+    int right1=right.first;
+    int combo=left.second+right.second;
+    pair<int,int> ans;
+    ans.first=max(left1,max(right1,combo));
+    ans.second=(max(left.second,right.second)+1);
+    return ans;
+ }
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        /*if(root==NULL){
+            return 0;
+        }
+        int left=diameterOfBinaryTree(root->left);
+        int right=diameterOfBinaryTree(root->right);
+        int combo=height(root->left)+height(root->right);
+        return max(left,max(right,combo));*/
+        return dfast(root).first;
+    }
+};
