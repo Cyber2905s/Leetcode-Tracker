b0f54c3d9a62ea197d9c501e67fe28759fa03429
Piyush Saini <piyushs2k4@gmail.com>
2025-09-12 22:12:39 +0000

    Sync LeetCode submission Runtime - 20 ms (6.99%), Memory - 18.9 MB (80.49%)

---
+++ my-submissions/3462-vowels-game-in-a-string/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        return ranges::any_of(s, [this](char c) { return isVowel(c); });
+    }
+    bool isVowel(char c) {
+    static constexpr string_view kVowels = "aeiou";
+    return kVowels.find(c) != string_view::npos;
+  }
+};
