35925febcbf659aac3149eeeb3d5dec8bf244f5f
Piyush Saini <piyushs2k4@gmail.com>
2026-02-26 15:37:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (82.24%)

---
+++ my-submissions/0216-combination-sum-iii/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int>ds;
+        comb(n,1,ds,k,ans);
+        return ans;
+    }
+    void comb(int sum, int last, vector<int>& ds, int k, vector<vector<int>>& ans){
+        if(sum==0 && ds.size()==k){
+            ans.push_back(ds);
+            return;
+        }
+        if(sum<=0 || ds.size()>k) return;
+        for(int i= last;i<=9;i++){
+            if(i<=sum){
+                ds.push_back(i);
+                comb(sum-i,i+1,ds,k,ans);
+                ds.pop_back();
+            }
+            else{
+                return;
+            }
+        }
+    }
+};
