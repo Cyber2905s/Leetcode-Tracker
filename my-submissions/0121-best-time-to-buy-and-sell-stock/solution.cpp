bf0d1faecb3bf9cb42197a17d35258d52198d1c6
Piyush Saini <piyushs2k4@gmail.com>
2026-06-08 14:58:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.3 MB (61.82%)

--- my-submissions/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ my-submissions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int s=prices.size();
-        int mini=INT_MAX;
-        int profit=0;
-        for(int i=0;i<s;i++){
-            mini=min(prices[i],mini);
-            profit=max(profit,prices[i]-mini);
+        int maxP = 0;
+        int minBuy = prices[0];
+
+        for (int& sell : prices) {
+            maxP = max(maxP, sell - minBuy);
+            minBuy = min(minBuy, sell);
         }
-        return profit;
-        
+        return maxP;
     }
 };
