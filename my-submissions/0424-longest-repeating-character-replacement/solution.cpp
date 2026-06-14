514f066bb73cbc3ffa7412eebfe3e0e212ee8831
Piyush Saini <piyushs2k4@gmail.com>
2026-06-14 14:36:32 +0000

    Sync LeetCode submission Runtime - 4 ms (56.68%), Memory - 11 MB (41.46%)

--- my-submissions/0424-longest-repeating-character-replacement/solution.cpp
+++ my-submissions/0424-longest-repeating-character-replacement/solution.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n= s.size();
-        int l=0,r=0;
+        unordered_map<char,int> count;
+        int res=0;
+        int l=0;
         int maxf=0;
-        int maxl=0;
-        vector <int>hashm (26,0);
-        while(r<n){
-            hashm[s[r]-'A']++;
-            maxf= max(maxf,hashm[s[r]-'A']);
-            if((r-l+1)-maxf > k){
-                hashm[s[l]-'A']--;
-                maxf=0;
+        for(int r=0;r<s.size();r++){
+            count[s[r]]++;
+            maxf=max(maxf,count[s[r]]);
+            while((r-l+1)-maxf>k){
+                count[s[l]]--;
                 l++;
             }
-            if((r-l+1)-maxf <= k){
-                maxl= max(r-l+1,maxl);
-            }
-            r++;
+            res=max(res,r-l+1);
         }
-        return maxl;
+        return res;
     }
 };
