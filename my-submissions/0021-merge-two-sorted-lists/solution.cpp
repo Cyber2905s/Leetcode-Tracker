114d3d958361c47a84610a8b8498575770d81b49
Piyush Saini <piyushs2k4@gmail.com>
2026-07-15 08:53:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (62.56%)

--- my-submissions/0021-merge-two-sorted-lists/solution.cpp
+++ my-submissions/0021-merge-two-sorted-lists/solution.cpp
@@ -8,47 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- ListNode* mergelist(ListNode* list1,ListNode* list2){
-    ListNode* curr=list1;
-    ListNode* next1=curr->next;
-    ListNode* temp=list2;
-    ListNode* temp1=temp->next;
-    if (next1==NULL) {
-        curr->next = temp;
-        return list1;
-    }
-    while(next1!=NULL && temp!=NULL){
-        if((curr->val<=temp->val) && (temp->val<=next1->val)){
-            temp1=temp->next;
-            curr->next=temp;
-            temp->next=next1;
-            curr=temp;
-            temp=temp1;
-        }
-        else{
-            curr=next1;
-            next1=next1->next;
-        }
-        if(next1==NULL){
-            curr->next=temp;
-        }
-    }
-    return list1;
- }
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL){
-            return list2;
-        } 
-        if(list2==NULL){
-            return list1;
+        ListNode dummy(0);
+        ListNode* node = &dummy;
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                node->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                node->next = list2;
+                list2 = list2->next;
+            }
+            node = node->next;
         }
-        if(list1->val<=list2->val){
-            return mergelist(list1,list2);
+        if(!list1){
+            node->next = list2;
         }
-        else{
-            return mergelist(list2,list1);
+        else if(!list2){
+            node->next = list1;
         }
+        return dummy.next;
     }
 };
