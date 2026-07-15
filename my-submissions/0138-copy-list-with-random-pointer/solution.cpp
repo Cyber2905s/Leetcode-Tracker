5b5baf43975b3bb43b678d5d055616e6c2a3c234
Piyush Saini <piyushs2k4@gmail.com>
2026-07-15 20:22:41 +0000

    Sync LeetCode submission Runtime - 7 ms (51.47%), Memory - 15 MB (87.60%)

--- my-submissions/0138-copy-list-with-random-pointer/solution.cpp
+++ my-submissions/0138-copy-list-with-random-pointer/solution.cpp
@@ -13,49 +13,37 @@ public:
     }
 };
 */
-void insertcopy(Node* head){
-    Node* temp=head;
-    while(temp!=NULL){
-        Node* nexele=temp->next;
-        Node* copy=new Node(temp->val);
-        copy->next=nexele;
-        temp->next=copy;
-        temp=nexele;
-    }
-}
-void conrandom(Node* head){
-    Node* temp=head;
-    while(temp!=NULL){
-        Node* copy=temp->next;
-        if(temp->random){
-            copy->random=temp->random->next;
-        }
-        else{
-            copy->random=NULL;
-        }
-        temp=temp->next->next;
-    }
-}
-Node* getdeepcopy(Node* head){
-    Node* temp = head;
-    Node* dummyNode = new Node(-1); 
-    Node* res = dummyNode;             
-    while(temp != NULL){
-        res->next = temp->next;
-        res = res->next;
-        temp->next = temp->next->next;
-        temp = temp->next;
-    }
-    return dummyNode->next;
-}
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(head==NULL){
-            return NULL;
+        if(head==nullptr){
+            return nullptr;
         }
-        insertcopy(head);
-        conrandom(head);
-        return getdeepcopy(head);
+        Node* l1 = head;
+        while(l1!=nullptr){
+            Node* l2 = new Node(l1->val);
+            l2->next = l1->next;
+            l1->next = l2;
+            l1 = l2->next;
+        }
+        Node* NewHead = head->next;
+        l1 = head;
+        while(l1!=nullptr){
+            if(l1->random != nullptr){
+                l1->next->random = l1->random->next;
+            }
+            l1 = l1->next->next;
+        }
+        l1 = head;
+        while(l1!=nullptr){
+            Node* l2 = l1->next;
+            l1->next = l2->next;
+            if(l2->next != nullptr){
+                l2->next = l2->next->next;
+            }
+            l1 = l1->next;
+        }
+        return NewHead;
     }
 };
