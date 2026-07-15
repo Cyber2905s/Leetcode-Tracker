9ff6f4b4aa0dd8a289299cc215646774414d5edd
Piyush Saini <piyushs2k4@gmail.com>
2026-07-15 09:35:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.8 MB (77.88%)

--- my-submissions/0143-reorder-list/solution.cpp
+++ my-submissions/0143-reorder-list/solution.cpp
@@ -10,45 +10,33 @@
  */
 class Solution {
 public:
-    ListNode* getmid(ListNode* head){
-        ListNode* fast=head->next;
-        ListNode* slow=head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        return slow;
-    }
-    ListNode* reverse(ListNode* head){
-        ListNode* curr=head;
-        ListNode* prev=NULL;
-        ListNode* next=NULL;
-        while(curr!=NULL){
-            next= curr->next;
-            curr->next = prev;
-            prev= curr;
-            curr=next;
-        }
-        return prev;
-    }
     void reorderList(ListNode* head) {
-        if(head->next==NULL){
-            return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* mid = getmid(head);
-        ListNode* head2=mid->next;
-        mid->next=NULL;
-        head2= reverse(head2);
-        ListNode* p1=head;
-        ListNode* p2=head2;
-        while(p2!=NULL){
-            ListNode* temp1= p1->next;
-            ListNode* temp2= p2->next;
-            p1->next=p2;
-            p2->next=temp1;
-            p1=temp1;
-            p2=temp2;
+
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+        ListNode* prev = nullptr;
+        while(second){
+            ListNode* front = second->next;
+            second->next = prev;
+            prev = second;
+            second = front;
         }
-        return;
+
+        ListNode* first = head;
+        second = prev;
+        while(second){
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
     }
 };
