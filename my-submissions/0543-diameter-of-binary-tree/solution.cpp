3dbba02da48d41df50f7eb7898f238d10bb01e88
Piyush Saini <piyushs2k4@gmail.com>
2026-07-21 16:14:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.8 MB (41.48%)

--- my-submissions/0543-diameter-of-binary-tree/solution.cpp
+++ my-submissions/0543-diameter-of-binary-tree/solution.cpp
@@ -9,39 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- /*int height(TreeNode* root){
-    if(root==NULL){
-        return 0;
-    }
-    int left=height(root->left);
-    int right=height(root->right);
-    return (max(left,right)+1);
- }*/
- //optimized solution
- pair<int,int> dfast(TreeNode* root){
-    if(root==NULL){
-        return make_pair(0,0);
-    }
-    pair<int,int> left=dfast(root->left);
-    pair<int,int> right=dfast(root->right);
-    int left1=left.first;
-    int right1=right.first;
-    int combo=left.second+right.second;
-    pair<int,int> ans;
-    ans.first=max(left1,max(right1,combo));
-    ans.second=(max(left.second,right.second)+1);
-    return ans;
- }
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        /*if(root==NULL){
+        int diameter = 0;
+        height(root,diameter);
+        return diameter;
+    }
+    int height(TreeNode* node, int& diameter){
+        if(!node){
             return 0;
         }
-        int left=diameterOfBinaryTree(root->left);
-        int right=diameterOfBinaryTree(root->right);
-        int combo=height(root->left)+height(root->right);
-        return max(left,max(right,combo));*/
-        return dfast(root).first;
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+        diameter = max(diameter,lh+rh);
+        return 1+max(lh,rh);
     }
 };
