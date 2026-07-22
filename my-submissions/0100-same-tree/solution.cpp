cae2c0d3d687d21111f04c345820ab3e2a40d856
Piyush Saini <piyushs2k4@gmail.com>
2026-07-22 09:45:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.9 MB (47.29%)

--- my-submissions/0100-same-tree/solution.cpp
+++ my-submissions/0100-same-tree/solution.cpp
@@ -12,23 +12,12 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL){
+        if(p == NULL && q == NULL){
             return true;
         }
-        if(p!=NULL && q==NULL){
+        if(p == NULL || q == NULL){
             return false;
         }
-        if(q!=NULL && p==NULL){
-            return false;
-        }
-        bool left=isSameTree(p->left,q->left);
-        bool right=isSameTree(p->right,q->right);
-        bool value=q->val==p->val;
-        if(left && right && value){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return((p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
     }
 };
