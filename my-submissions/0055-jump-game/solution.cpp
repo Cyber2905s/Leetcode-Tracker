1110cfb16c1f3b856c53fd0f2b4e3a5a9682bb5e
Piyush Saini <piyushs2k4@gmail.com>
2026-07-29 13:15:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 52.4 MB (26.02%)

--- my-submissions/0055-jump-game/solution.cpp
+++ my-submissions/0055-jump-game/solution.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxi;
+        int maxInd = 0;
         for(int i=0;i<nums.size();i++){
-            if(i>maxi) return false;
-            maxi=max(maxi,i+nums[i]);
+            if(i>maxInd){
+                return false;
+            }
+            maxInd = max(maxInd,nums[i]+i);
         }
         return true;
     }
