daa1769316a7d09e66f6a5b16b5d796408caa9e0
Piyush Saini <piyushs2k4@gmail.com>
2026-08-03 17:27:04 +0000

    Sync LeetCode submission Runtime - 58 ms (37.54%), Memory - 61.8 MB (39.26%)

---
+++ my-submissions/0131-palindrome-partitioning/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> path;
+        solve(0,s,ans,path);
+        return ans;
+    }
+
+    void solve(int ind,string s,vector<vector<string>> &ans,vector<string> &path){
+        if(ind==s.size()){
+            ans.push_back(path);
+            return;
+        }
+        for(int i =ind;i<s.size();i++){
+            if(isPal(s,ind,i)){
+                path.push_back(s.substr(ind,i-ind+1));
+                solve(i+1,s,ans,path);
+                path.pop_back();
+            }
+        }
+    }
+
+    bool isPal(string s,int start,int end){
+        while(start<=end){
+            if(s[start]==s[end]){
+                start++;
+                end--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
