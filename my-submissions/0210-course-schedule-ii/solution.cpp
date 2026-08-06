133ec87d1c514df71e2f0b61385a88ca35c31d5a
Piyush Saini <piyushs2k4@gmail.com>
2026-08-06 15:04:39 +0000

    Sync LeetCode submission Runtime - 7 ms (34.46%), Memory - 18.1 MB (82.78%)

---
+++ my-submissions/0210-course-schedule-ii/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> inDeg(numCourses,0);
+        for(auto &it: prerequisites){
+            adj[it[1]].push_back(it[0]);
+            inDeg[it[0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(inDeg[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it: adj[node]){
+                inDeg[it]--;
+                if(inDeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()<numCourses) return {};
+        return topo;
+    }
+};
