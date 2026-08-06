4f050984c17b4d2ca0bdfb784af047314ece0803
Piyush Saini <piyushs2k4@gmail.com>
2026-08-06 11:45:00 +0000

    Sync LeetCode submission Runtime - 3 ms (76.42%), Memory - 19.3 MB (56.55%)

---
+++ my-submissions/0207-course-schedule/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    bool dfs(int node,vector<vector<int>>& adj,vector<int> &vis,vector<int> &path){
+        vis[node]=1;
+        path[node]=1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                if(dfs(it,adj,vis,path)) return true;
+            }
+            else if(path[it]){
+                return true;
+            }
+        }
+        path[node]=0;
+        return false;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(auto &pre: prerequisites){
+            adj[pre[1]].push_back(pre[0]);
+        }
+        vector<int> vis(numCourses,0);
+        vector<int> path(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,path)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
