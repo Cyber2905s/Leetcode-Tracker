81fc684523e9151b65c6e6200e544f17002d1d06
Piyush Saini <piyushs2k4@gmail.com>
2026-08-07 12:57:29 +0000

    Sync LeetCode submission Runtime - 114 ms (50.33%), Memory - 116.8 MB (95.40%)

---
+++ my-submissions/0219-contains-duplicate-ii/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_set<int> set;
+        for(int i=0;i<n;i++){
+            if(i>k){
+                set.erase(nums[i-k-1]);
+            }
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
