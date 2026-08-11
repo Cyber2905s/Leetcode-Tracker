ca6e656ae27c47dbe10ad47934826058a1e31001
Piyush Saini <piyushs2k4@gmail.com>
2026-08-11 19:56:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.4 MB (34.75%)

---
+++ my-submissions/0803-cheapest-flights-within-k-stops/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it: flights){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        vector<int> price(n,1e9);
+        price[src]=0;
+        queue<pair<int,pair<int, int>>> q; //stops,{node,price}
+        q.push({0,{src,0}});
+        while(!q.empty()){
+            int stop = q.front().first;
+            int node = q.front().second.first;
+            int cost = q.front().second.second;
+            q.pop();
+            if(stop>k) continue;
+            for(auto it: adj[node]){
+                int adjNode = it.first;
+                int money = it.second;
+                if(cost+money<price[adjNode] && stop<=k){
+                    price[adjNode] = cost+money;
+                    q.push({stop+1,{adjNode,price[adjNode]}});
+                }
+            }
+        }
+        if(price[dst]==1e9) return -1;
+        return price[dst];
+    }
+};
