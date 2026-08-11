c69788f5bce4da5636ac4791fb48a45342b8133f
Piyush Saini <piyushs2k4@gmail.com>
2026-08-11 18:33:45 +0000

    Sync LeetCode submission Runtime - 53 ms (58.98%), Memory - 24.6 MB (57.09%)

---
+++ my-submissions/1753-path-with-minimum-effort/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+        priority_queue<pair<int, pair<int, int>>,vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<vector<int>> dis(n,vector<int>(m,1e9));
+        dis[0][0]=0;
+        pq.push({0,{0,0}});
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
+        while(!pq.empty()){
+            int diff = pq.top().first;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
+            pq.pop();
+            if(row==n-1 && col==m-1) return diff;
+            for(int i=0;i<4;i++){
+                int nrow = row+drow[i];
+                int ncol = col+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    int newEffort = max(abs(heights[nrow][ncol]-heights[row][col]),diff);
+                    if(newEffort<dis[nrow][ncol]){
+                        dis[nrow][ncol]=newEffort;
+                        pq.push({dis[nrow][ncol],{nrow,ncol}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
