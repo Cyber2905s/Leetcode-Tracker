9ed62de4cdcd13e576a16cdef38d0ce99e63c7b4
Piyush Saini <piyushs2k4@gmail.com>
2026-08-11 20:16:25 +0000

    Sync LeetCode submission Runtime - 90 ms (35.96%), Memory - 48 MB (27.50%)

---
+++ my-submissions/0744-network-delay-time/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto it: times){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        vector<int> time(n+1,1e9);
+        time[k]=0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0,k});
+        while(!pq.empty()){
+            int node = pq.top().second;
+            int times = pq.top().first;
+            pq.pop();
+            for(auto it: adj[node]){
+                int adjNode = it.first;
+                int nodeTime = it.second;
+                if(times+nodeTime < time[adjNode]){
+                    time[adjNode] = times+nodeTime;
+                    pq.push({time[adjNode],adjNode});
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++){
+            if(time[i]==1e9) return -1;
+        }
+        int ans = 0;
+        for(int i=1;i<n+1;i++){
+            ans = max(time[i],ans);
+        }
+        return ans;
+    }
+};
