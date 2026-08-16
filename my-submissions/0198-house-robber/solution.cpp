079b2e83559cfc8a59b9e996f4e4ccf8997bc079
Piyush Saini <piyushs2k4@gmail.com>
2026-08-16 14:38:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (79.41%)

--- my-submissions/0198-house-robber/solution.cpp
+++ my-submissions/0198-house-robber/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-       int n=nums.size();
-       if(n==0) return 0;
-       if(n==1) return nums[0];
-       vector<int> dp(n,0);
-       dp[0]=nums[0];
-       dp[1]=max(nums[0],nums[1]);
-       for(int i=2;i<n;i++){
-        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
-       }
-       return dp[n-1];
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=prev2;
+            int ntake = 0+prev;
+            int curr = max(take,ntake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 };
