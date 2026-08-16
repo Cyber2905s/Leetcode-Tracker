626a8611cfc334b1861a9cb3f255259f1f882c1f
Piyush Saini <piyushs2k4@gmail.com>
2026-08-16 20:59:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.8 MB (76.13%)

---
+++ my-submissions/0064-minimum-path-sum/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) curr[j]=grid[i][j];
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i>0) up = prev[j];
+                    else up+=1e9;
+                    if(j>0) left = curr[j-1];
+                    else left+=1e9;
+                    curr[j] = grid[i][j]+min(up,left);
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
+};
