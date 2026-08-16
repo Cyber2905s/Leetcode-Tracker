c70ada9f902ea43418ab0777ea676bd04cae1aef
Piyush Saini <piyushs2k4@gmail.com>
2026-08-16 20:43:34 +0000

    Sync LeetCode submission Runtime - 1 ms (15.31%), Memory - 11.6 MB (84.49%)

---
+++ my-submissions/0063-unique-paths-ii/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1) return 0;
+        vector<int> prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) curr[j]=1;
+                else if(obstacleGrid[i][j]==1) curr[j]=0;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = up+left;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
+};
