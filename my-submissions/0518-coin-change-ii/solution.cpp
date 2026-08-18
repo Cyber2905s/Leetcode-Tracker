436e69252129919625e81dba5a5db8d624d18aac
Piyush Saini <piyushs2k4@gmail.com>
2026-08-18 21:07:08 +0000

    Sync LeetCode submission Runtime - 25 ms (70.66%), Memory - 10.2 MB (93.55%)

---
+++ my-submissions/0518-coin-change-ii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<unsigned int> prev(amount+1,0), curr(amount+1,0);
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0) prev[i] = 1;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                unsigned int notTake = prev[target];
+                unsigned int take = 0;
+                if(coins[ind]<=target) take = curr[target-coins[ind]];
+                curr[target] = take+notTake;
+            }
+            prev = curr;
+        }
+        return (int)prev[amount];
+    }
+};
