dfcbe8b8bd436e7419144af32c3897ae2e21aab6
Piyush Saini <piyushs2k4@gmail.com>
2026-08-21 18:39:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.6 MB (88.94%)

--- my-submissions/0072-edit-distance/solution.cpp
+++ my-submissions/0072-edit-distance/solution.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int m= word1.length();
-        int n= word2.length();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        for(int i=0;i<=m;i++){
-            //deleting from word 1
-            dp[i][0]=i; 
-        }
-        for(int i=0;i<=n;i++){
-            //inserting in word 1
-            dp[0][i]=i; 
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
+        int n = word1.size();
+        int m = word2.size();
+        vector<int> prev(m+1,0),curr(m+1,0);
+        for(int i=0;i<=m;i++) prev[i] = i;
+        for(int i=1;i<=n;i++){
+            curr[0] = i;
+            for(int j=1;j<=m;j++){
                 if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else{
-                    int insertop = dp[i][j-1];
-                    int deleteop = dp[i-1][j];
-                    int exchangeop = dp[i-1][j-1];
-                    dp[i][j]=1+min({insertop,deleteop,exchangeop});
+                    curr[j] = 1+min(prev[j],min(curr[j-1],prev[j-1]));
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return prev[m];
     }
 };
