80a8c404cb8f61aa1dbdcb3121cdc1f00cb988ba
Piyush Saini <piyushs2k4@gmail.com>
2026-08-26 17:29:57 +0000

    Sync LeetCode submission Runtime - 1514 ms (31.40%), Memory - 9.3 MB (77.93%)

---
+++ my-submissions/0132-palindrome-partitioning-ii/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPal(int i,int j,string &s){
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++,j--;
+        }
+        return true;
+    }
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--){
+            int mini = 1e9;
+            for(int j=i;j<n;j++){
+                if(isPal(i,j,s)){
+                    int cost = 1+dp[j+1];
+                    mini = min(cost,mini);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0]-1;
+    }
+};
