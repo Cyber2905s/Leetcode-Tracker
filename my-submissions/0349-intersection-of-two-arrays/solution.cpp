bbff3a19d2908151f66f708897e7350e2eefdd20
Piyush Saini <piyushs2k4@gmail.com>
2026-08-27 19:19:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.1 MB (66.58%)

---
+++ my-submissions/0349-intersection-of-two-arrays/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st(nums1.begin(),nums1.end());
+        vector<int> ans;
+        for(int num:nums2){
+            if(st.erase(num)){
+                ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+};
