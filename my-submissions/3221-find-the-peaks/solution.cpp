97744df38be66e318f281a1ea64ea79f95ea7d1d
Piyush Saini <piyushs2k4@gmail.com>
2026-09-07 22:28:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 24.7 MB (7.34%)

---
+++ my-submissions/3221-find-the-peaks/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int> ans;
+        int n = mountain.size();
+        int i = 1;
+        while(i<n-1){
+            if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]){
+                ans.push_back(i);
+                i+=2;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+    }
+};
