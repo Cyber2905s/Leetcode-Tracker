ffecc36d84cdb9269a84130c5a7e94baba738fd8
Piyush Saini <piyushs2k4@gmail.com>
2026-09-14 10:44:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.2 MB (95.56%)

--- my-submissions/0222-count-complete-tree-nodes/solution.cpp
+++ my-submissions/0222-count-complete-tree-nodes/solution.cpp
@@ -10,11 +10,29 @@
  * };
  */
 class Solution {
+private:
+    int fhl(TreeNode* node){
+        int h =0;
+        while(node){
+            h++;
+            node = node->left;
+        }
+        return h;
+    }
+    int fhr(TreeNode* node){
+        int h =0;
+        while(node){
+            h++;
+            node = node->right;
+        }
+        return h;
+    }
 public:
     int countNodes(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        return (1+countNodes(root->left)+countNodes(root->right));
+        if(!root) return 0;
+        int lh = fhl(root);
+        int rh = fhr(root);
+        if(rh==lh) return (1<<lh)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);        
     }
 };
