64b3887c8cd1fce89bb33d31e81d7f70864124fa
Piyush Saini <piyushs2k4@gmail.com>
2026-09-22 12:38:42 +0000

    Sync LeetCode submission Runtime - 45 ms (54.57%), Memory - 66.3 MB (20.95%)

--- my-submissions/0820-find-eventual-safe-states/solution.cpp
+++ my-submissions/0820-find-eventual-safe-states/solution.cpp
@@ -1,39 +1,35 @@
 class Solution {
-private:
-    bool dfs(int node,vector<vector<int>>& adj,vector<int> &vis,vector<int> &path,vector<int> &check){
-        vis[node]=1;
-        path[node]=1;
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                if(dfs(it,adj,vis,path,check)) {
-                    return true;
-                }
-            }
-            else if(path[it]){
-                return true;
-            }
-        }
-        check[node]=1;
-        path[node]=0;
-        return false;
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> vis(n,0);
-        vector<int> path(n,0);
-        vector<int> check(n,0);
+        int V = graph.size();
+        vector<vector<int>> adjRev(V);
+        vector<int> inDeg(V,0);
+        //Reverse the adj list
+        for(int i=0;i<V;i++){
+            for(auto it: graph[i]){
+                adjRev[it].push_back(i);
+                inDeg[i]++;
+            }
+        }
         vector<int> safe;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,graph,vis,path,check);
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(inDeg[i]==0){
+                q.push(i);
             }
         }
-        for(int i=0;i<n;i++){
-            if(check[i]){
-                safe.push_back(i);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safe.push_back(node);
+            for(auto it: adjRev[node]){
+                inDeg[it]--;
+                if(inDeg[it]==0){
+                    q.push(it);
+                }
             }
         }
+        sort(safe.begin(),safe.end());
         return safe;
     }
 };
